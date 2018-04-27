5bf8cb87c48db83f017500e2de014a180f16a355
Emanuel Ciuca <emanuel@webgenerals.com>
2018-04-27 06:54:27 +0200

    Add bash completion

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ cp wgcli /usr/local/wgcli/wgcli
 chmod 755 /usr/local/wgcli/wgcli
 
 ln -s /usr/local/wgcli/wgcli /usr/bin/wgcli
+complete -W "build-jar create-docker-image deploy-jar deploy-docker-image" wgcli
